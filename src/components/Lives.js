8ec6be642b01e86b04b4ad41987732eae7b2c721
tsubasa_kai_0321 <fbx7h7kt7r@privaterelay.appleid.com>
2025-11-18 02:23:49 +0000

    UQ　mobile

--- src/components/Lives.js
+++ src/components/Lives.js
@@ -1,19 +1,13 @@
 import React from "react";
 
 export default function Lives({ lives }) {
-  console.log("残機:", lives); // ← デバッグ用表示
+  console.log("残機:", lives);
+
   const hearts = Array.from({ length: lives }, (_, i) => (
     <span key={i} style={{ fontSize: "1.8rem", margin: "0 2px" }}>
       ❤️
     </span>
   ));
-  return (
-    <div style={{ textAlign: "center", marginTop: "10px" }}>
-      {lives > 0 ? (
-        hearts
-      ) : (
-        <span style={{ color: "red" }}>💀 GAME OVER 💀</span>
-      )}
-    </div>
-  );
+
+  return <div style={{ textAlign: "center", marginTop: "10px" }}>{hearts}</div>;
 }
